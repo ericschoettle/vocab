ffd458216a434aff3d8dc33163dfc62e8127ba04
ericschoettle <eric.schoettle@gmail.com>
2017-04-28 10:03:05 -0700

    definitions pass specs

--- spec/definitions_spec.rb
+++ spec/definitions_spec.rb
@@ -1,51 +1,51 @@
 require('rspec')
-require('definition')
+require('definitions')
 
 
-describe('Definition') do
-  definition_spec_params = {"word" => "slither", ":part_of_speech" => "verb", "in_a_sentence" => "The snake slithered across the path", "body" => "to go or walk with a sliding motion"}
+describe('Definitions') do
+  definition_spec_params = {"word" => "slither", "part_of_speech" => "verb", "in_a_sentence" => "The snake slithered across the path", "body" => "to go or walk with a sliding motion"}
   definition_spec_params2 = {"word" => "slither", ":part_of_speech" => "verb", "in_a_sentence" => "The box slithered down the chute", "body" => "to slide down or along a surface, especially unsteadily, from side to side, or with some friction or noise"}
   before() do
-    Definition.clear()
+    Definitions.clear()
   end
 
   describe("#word") do
     it("returns the word associated with the definition") do
-      test_definition = Definition.new(definition_spec_params)
+      test_definition = Definitions.new(definition_spec_params)
       expect(test_definition.word()).to(eq("slither"))
     end
   end
 
   describe("#part_of_speech") do
     it("returns the part of speech of the definition") do
-      test_definition = Definition.new(definition_spec_params)
+      test_definition = Definitions.new(definition_spec_params)
       expect(test_definition.part_of_speech()).to(eq("verb"))
     end
   end
 
   describe("#in_a_sentence") do
     it("returns a sentence using the word in context") do
-      test_definition = Definition.new(definition_spec_params)
+      test_definition = Definitions.new(definition_spec_params)
       expect(test_definition.in_a_sentence()).to(eq("The snake slithered across the path"))
     end
   end
 
   describe("#body") do
     it("returns the definition") do
-      test_definition = Definition.new(definition_spec_params)
+      test_definition = Definitions.new(definition_spec_params)
       expect(test_definition.body()).to(eq("to go or walk with a sliding motion"))
     end
   end
 
   describe(".all") do
     it("is empty at first") do
-      expect(Definition.all()).to(eq([]))
+      expect(Definitions.all()).to(eq([]))
     end
   end
 
   describe("#id") do
     it "returns the id of the definition" do
-      test_definition = Definition.new(definition_spec_params)
+      test_definition = Definitions.new(definition_spec_params)
       test_definition.save()
       expect(test_definition.id()).to(eq(1))
     end
@@ -53,27 +53,27 @@ describe('Definition') do
 
   describe("#save") do
     it("adds a definition to the array of saved definitions") do
-      test_definition = Definition.new(definition_spec_params)
+      test_definition = Definitions.new(definition_spec_params)
       test_definition.save()
-      expect(Definition.all()).to(eq([test_definition]))
+      expect(Definitions.all()).to(eq([test_definition]))
     end
   end
 
   describe(".clear") do
     it("empties out all of the saved definitions") do
-      Definition.new(definition_spec_params).save()
-      Definition.clear()
-      expect(Definition.all()).to(eq([]))
+      Definitions.new(definition_spec_params).save()
+      Definitions.clear()
+      expect(Definitions.all()).to(eq([]))
     end
   end
 
   describe('find') do
     it ('returns definition based on id #') do
-      test_definition = Definition.new(definition_spec_params)
+      test_definition = Definitions.new(definition_spec_params)
       test_definition.save()
-      test_definition2 = Definition.new(definition_spec_params)
+      test_definition2 = Definitions.new(definition_spec_params)
       test_definition2.save()
-      expect(Definition.find(test_definition.id())).to(eq(test_definition))
+      expect(Definitions.find(test_definition.id())).to(eq(test_definition))
     end
   end
 end
