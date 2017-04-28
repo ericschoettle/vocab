97690b886b20996e6ec0d3038ca4101da824abc7
ericschoettle <eric.schoettle@gmail.com>
2017-04-28 10:25:47 -0700

    pass words spec

--- spec/words_spec.rb
+++ spec/words_spec.rb
@@ -31,14 +31,15 @@ describe('Words') do
 
   describe("#save") do
     it("adds a word to the array of saved words") do
-      Words.new("slither").save()
-      expect(Words.all()).to(eq([test_word]))
+      test_word = Words.new("slither").save()
+      expect(Words.all()).to(eq(test_word))
     end
   end
 
   describe("#add_definition") do
     it "adds a definition to the word" do
-      test_word = Words.new("slither").save()
+      test_word = Words.new("slither")
+      test_word.save()
       test_definition = Definitions.new({"word" => "slither", "part_of_speech" => "verb", "in_a_sentence" => "The snake slithered across the path", "body" => "to go or walk with a sliding motion"}).save()
       test_word.add_definition(test_definition)
       expect(test_word.definitions()).to(eq([test_definition]))
