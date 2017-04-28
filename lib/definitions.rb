f9a1ecd25c0c912e4ee5dd8d7f1df30a55e454a0
ericschoettle <eric.schoettle@gmail.com>
2017-04-28 09:56:57 -0700

    add definitions logic and spec

---
+++ lib/definitions.rb
@@ -0,0 +1,36 @@
+require('pry')
+class Definitions
+  @@definitions = []
+  attr_accessor(:word, :part_of_speech, :in_a_sentence, :body)
+  attr_reader(:id)
+
+  define_method(:initialize) do |params|
+    @word = params["word"]
+    @part_of_speech = params["part_of_speech"]
+    @in_a_sentence = params["in_a_sentence"]
+    @body = params["body"]
+    @id = @@definitions.length() + 1
+  end
+
+  define_singleton_method(:all) do
+    @@definitions
+  end
+
+  define_method(:save) do
+    @@definitions.push(self)
+  end
+
+  define_singleton_method(:clear) do
+    @@definitions = []
+  end
+
+  define_singleton_method(:find) do |ident|
+    found_definition = nil
+    @@definitions.each() do |definition|
+      if definition.id()==ident.to_i
+        found_definition = definition
+      end
+    end
+    found_definition
+  end
+end
